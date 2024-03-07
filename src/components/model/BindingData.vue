037aaafdb9319f2e5a6d00538d737ee1ea24c8dd
AkakiyBerula <vlad5dyakun.gmail.com>
2024-03-07 14:43:19 +0200

    Двонаправлений Data Binding (доповнення)

--- src/components/model/BindingData.vue
+++ src/components/model/BindingData.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <v-text-field v-model="text" label="Binding"></v-text-field>
+    <v-text-field v-model="text" label="Text"></v-text-field>
     <p>Значення текстового поля: {{ text }}</p>
+    <v-text-field v-model="number" label="Number"></v-text-field>
+    <p>Значення числового поля: {{ number }}</p>
+
+    <v-checkbox v-model="checkbox" label="Очистити поля" @change="clearFields"></v-checkbox>
   </div>
 </template>
 
@@ -10,12 +14,23 @@ export default {
   name: "BindingData",
   data() {
     return {
-      text: ''
+      text: '',
+      number: null,
+      checkbox: false
     };
+  },
+  methods: {
+    clearFields() {
+      if (this.checkbox) {
+        this.text = '';
+        this.number = null;
+      }
+    }
   },
   mounted() {
     setTimeout(() => {
       this.text = 'Несподіване втручання!';
+      this.number = 42
     }, 3000)
   }
 };
